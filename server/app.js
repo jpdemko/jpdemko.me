efdf9923845cbb224ca3b36bb376f683fdbe5e17
Joseph P Demko <prestondemko@gmail.com>
2021-02-12 12:27:28 -0600

    Moved/changed files and folders for Heroku deploy.

--- server/app.js
+++ server/app.js
@@ -1,56 +1,109 @@
-require("dotenv").config({ path: "./.env.local" })
-
 const express = require("express")
-const app = express()
 const session = require("express-session")
-const db = require("./db/db")
 const pgSession = require("connect-pg-simple")(session)
-const passport = require("./passport")
 const cookieParser = require("cookie-parser")
 const morgan = require("morgan")
 const cors = require("cors")
 const helmet = require("helmet")
+const debug = require("debug")("backend:server")
+// Heroku packages for optimization?
+const cluster = require("cluster")
+const numCPUs = require("os").cpus().length
 
-app.use(morgan("dev"))
-app.use(helmet())
-app.use(
-	cors({
-		origin: [process.env.ORIGIN_URL, process.env.SERVER_URL],
-		credentials: true,
+const dev = process.env.NODE_ENV !== "production"
+let app = null
+
+if (!dev && cluster.isMaster) {
+	console.error(`Node cluster master ${process.pid} is running!`)
+	// Fork workers.
+	for (let i = 0; i < numCPUs; i++) {
+		cluster.fork()
+	}
+	cluster.on("exit", (worker, code, signal) => {
+		console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`)
 	})
-)
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser(process.env.SESSION_SECRET))
-const sessionMiddleware = session({
-	name: "sessionID",
-	store: new pgSession({ pool: db }),
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-		...(app.get("env") === "production" && { secure: true }),
-	},
-})
-app.use(sessionMiddleware)
-app.use(passport.initialize())
-app.use(passport.session())
+} else {
+	if (dev) require("dotenv").config({ path: "./.env.local" })
+	const db = require("./db/db")
+	const passport = require("./passport")
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"))
-
-	const path = require("path")
-	app.get("*", function (req, res) {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	app = express()
+	app.use(morgan(dev ? "dev" : "common"))
+	app.use(helmet())
+	app.use(
+		cors({
+			origin: [process.env.ORIGIN_URL, process.env.SERVER_URL],
+			credentials: true,
+		})
+	)
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: false }))
+	app.use(cookieParser(process.env.SESSION_SECRET))
+	const sessionMiddleware = session({
+		name: "sessionID",
+		store: new pgSession({ pool: db }),
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+			...(dev && { secure: true }),
+		},
 	})
-}
+	app.use(sessionMiddleware)
+	app.use(passport.initialize())
+	app.use(passport.session())
 
-app.use("/", require("./routes/index"))
-app.use("/auth", require("./routes/auth"))
-app.use("/weather", require("./routes/weather"))
+	if (!dev) {
+		const path = require("path")
+		app.use(express.static(path.resolve(__dirname, "../client/build")))
+		app.get("*", function (req, res) {
+			res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+		})
+	}
 
-module.exports = {
-	main: app,
-	sessionMiddleware,
+	app.use("/", require("./routes/index"))
+	app.use("/auth", require("./routes/auth"))
+	app.use("/weather", require("./routes/weather"))
+
+	const server = require("http").createServer(app)
+	const port = process.env.PORT || 5000
+	app.set("port", port)
+
+	// @ts-ignore
+	const io = require("socket.io")(server, {
+		cors: {
+			origin: [process.env.ORIGIN_URL, process.env.SERVER_URL],
+			methods: ["GET", "POST"],
+			credentials: true,
+		},
+	})
+	require("./socketAPI")(io, sessionMiddleware)
+
+	server.listen(port)
+	server.on("error", onError)
+	server.on("listening", onListening)
+
+	function onError(error) {
+		if (error.syscall !== "listen") throw error
+
+		const bind = typeof port === "string" ? "Pipe " + port : "Port " + port
+		// Handle specific listen errors with friendly messages.
+		switch (error.code) {
+			case "EACCES":
+				console.error(bind + " requires elevated privileges")
+				process.exit(1)
+			case "EADDRINUSE":
+				console.error(bind + " is already in use")
+				process.exit(1)
+			default:
+				throw error
+		}
+	}
+
+	function onListening() {
+		const addr = server.address()
+		const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port
+		debug("Listening on " + bind)
+	}
 }
